b6d78fb78a4578e6834f426d742397e37d4aaa33
Isaiah Odhner <isaiahodhner@gmail.com>
2014-01-31 03:53:21 -0500

    Go!
    It can whistle 99 bottles of beer, but I haven't tried much else. The
    stop button doesn't work.

---
+++ app.js
@@ -0,0 +1,85 @@
+
+global = window;
+
+AudioContext = global.AudioContext || global.webkitAudioContext;
+
+var REST = null;
+
+$(function(){
+	
+	var $prog = $("textarea");
+	var $output = $("output");
+	var output = function(_){$output.text(_).removeClass("error");};
+	var error = function(_){$output.text(_).addClass("error");};
+	
+	var smooth = true;
+	$("[value=smooth]").prop("checked",true);
+	$(":radio").on("change",function(){
+		smooth = $("[value=smooth]").is(":checked");
+		console.log(smooth);
+	});
+	
+	var actx, osc, gain;
+	if(AudioContext){
+		actx = new AudioContext();
+		
+		gain = actx.createGain();
+		gain.connect(actx.destination);
+		//gain.gain.value = 0.1;
+		gain.gain.setValueAtTime(0,actx.currentTime);
+		
+		osc = actx.createOscillator();
+		osc.start(0);
+		osc.connect(gain);
+	}else{
+		error("No AudioContext! This will be boring.");
+	}
+	//$prog.on("change",
+	$("#play").on("click", function(){
+		try{
+			var prog = $prog.val();
+			var song = choon(prog);
+			output("");
+			play(song);
+		}catch(e){
+			error(e);
+		}
+	});
+	$("#stop").on("click", function(){
+		stop();
+	});
+	
+	function play(song){
+		stop();
+		var songStartTime = actx.currentTime;
+		var noteLength = 0.05;//100ms
+		var base = 440;//A
+		for(var i=0; i<=song.length; i++){
+			var t = songStartTime + i * noteLength;
+			var note = song[i];
+			if(note == REST){
+				if(smooth){
+					gain.gain.linearRampToValueAtTime(0, t + noteLength*0.2);
+				}else{
+					gain.gain.setValueAtTime(0, t + noteLength*0.2);
+				}
+			}else{
+				var freq = Math.pow(2, Math.log(base) / Math.LN2 + note/12);
+				if(smooth){
+					osc.frequency.exponentialRampToValueAtTime(freq, t);
+					gain.gain.linearRampToValueAtTime(0.1, t);
+				}else{
+					osc.frequency.setValueAtTime(freq, t);
+					gain.gain.setValueAtTime(0.1, t);
+				}
+			}
+		}
+	}
+	function stop(){
+		osc.frequency.cancelScheduledValues(actx.currentTime);
+		gain.gain.cancelScheduledValues(actx.currentTime);
+		gain.gain.setValueAtTime(0,actx.currentTime);
+		gain.gain.value = 0;
+	}
+	
+});
---
+++ choon.js
@@ -0,0 +1,149 @@
+// Choon interpreter
+
+choon = function(prog){
+	var REST = null;
+	
+	var tokens = [];
+	var tokenizer = /([A-G][#b]?)|([~%?\-+.])|(=?[a-z]+)|(=[-\d]\d*)|(:\|\|)|(\|\|:)|(\/\/.*)/gm;
+	prog.replace(tokenizer, function(m){
+		tokens.push(m);
+	});
+	var transpose = 0;
+	var lastval = 0;
+	var output = [];
+	var repeatstarts = [];
+	var repeatcounts = [];
+	var markers = {};
+	var lastmarker = {};
+
+	var tokeni = 0;
+	while(tokens[tokeni]){
+		var t = tokens[tokeni];
+		if(t.match(/^[A-G]/)){
+			var val = "C-D-EF-G-A-B".indexOf(t[0]) - 9;
+			if(t[1]){
+				if(t[1] == "#"){
+					if(val == 2){
+						val = -9;
+					}else{
+						val += 1;
+					}
+				}else if(t[1] == "b"){
+					if(val == -9){
+						val = 2;
+					}else{
+						val -= 1;
+					}
+				}
+			}
+			lastval = val + transpose;
+			output.push(lastval);
+		}else if(t.match(/^[a-z]+/)){
+			if(markers[t]){ // save for the x=x case
+				lastmarker = {t: markers[t]};
+			}
+			markers[t] = output.length;
+		}else if(t.match(/^=-?\d+/)){
+			var tp = t.substr(1);//t[1..-1]
+			lastval = output[tp-1] + transpose;
+			output.push(lastval);
+		}else if(t.match(/^=[a-z]+/)){
+			var tp = t.substr(1);//t[1..-1]
+			var m = markers[tp];
+			if(m && m < output.length){
+				lastval = output[m] + transpose;
+			}else if(lastmarker[tp]){
+				lastval = output[lastmarker[tp]] + transpose;
+			}else{
+				throw new Error('Marker "'+tp+'" was used before being set.');
+			}
+			output.push(lastval);
+		}else{
+			switch(t){
+				case "-":
+					if(lastval) transpose -= lastval;
+					break;
+				case "+":
+					if(lastval) transpose += lastval;
+					break;
+				case ".":
+					transpose = 0;
+					break;
+				case "||:":
+					if(lastval == REST || lastval > 0){
+						repeatstarts.push(tokeni);
+						repeatcounts.push(lastval?lastval:-1);// nil means forever (what does that mean?)
+					}else{ // jump to end of this repeat
+						incounts = 1;
+						while(tokens[tokeni] && incounts > 0){
+							while(tokens[tokeni] && tokens[tokeni] != ":||"){
+								if(tokens[tokeni] == "||:"){
+									incounts++;
+								}
+								tokeni++;
+							}
+							incounts--;
+						}
+					}
+					break;
+				case ":||":
+					repeatcounts[repeatcounts.length-1]--;
+					if(repeatcounts[repeatcounts.length-1] == 0){
+						repeatstarts.pop();
+						repeatcounts.pop();
+					}else{
+						tokeni = repeatstarts[repeatstarts.length-1];
+					}
+					break;
+				case "~":
+					if(lastval == 0){
+						incounts = 1;
+						while(tokens[tokeni] && incounts > 0){
+							while(tokens[tokeni] && tokens[tokeni] != ":||"){
+								if(tokens[tokeni] == "||:"){
+									incounts++;
+								}
+								tokeni++;
+							}
+							incounts--;
+						}
+						repeatstarts.pop();
+						repeatcounts.pop();
+					}
+					break;
+				case "%":
+					output.push(REST);
+					lastval = REST;
+					break;
+				case "?":
+					var n, used=[];
+					for(var _=0;_<12;_++){
+						while(1){
+							n = rand(12) - 9 + transpose;
+							if(used.indexOf(n) == -1) break;
+						}
+						used.push(n);
+						output.push(n);
+					}
+					lastval = output[output.length-1];
+					break;
+			} // end switch
+		}
+		tokeni++;
+	}
+	
+	/*Original output method:
+		var s = ">5000";
+		output.forEach(function(v,i,a){
+			s += "\nT100;"+(v==REST?"":v);
+		});
+		return s + ".\n";
+	*/
+
+	return output;
+
+
+	function rand(max){
+		return(Math.random()*max)|0;
+	}
+};
